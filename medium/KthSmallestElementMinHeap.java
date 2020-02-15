9dba7a1d1d4d2777a77475f0ea6f68b75dfb54ea
Pushpanjay Kumar <Pushpanjaykumar@gmail.com>
2020-02-15 20:09:33 +0530

    kth smallest using minHeap

---
+++ medium/KthSmallestElementMinHeap.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KthSmallestElementMinHeap{
+
+    private static int kthSmallest(int arr[], int k){
+        MinHeap heap = new MinHeap(arr, arr.length);
+        while(k>1){
+            heap.extractMin();
+            k--;
+        }
+        return heap.extractMin();
+    }
+
+    // TC = O(n + klogN)
+    public static void main(String... args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int s = Integer.parseInt(br.readLine());
+        
+        int arr[] = new int[s];
+        int i=0;
+        String in[] = br.readLine().trim().split(",");
+        for(String sin:in){
+            arr[i++] = Integer.parseInt(sin.trim());
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        int r = kthSmallest(arr, k);
+        System.out.println(r);
+    }
+}
+
+class MinHeap{
+    int arr[];
+    int c;
+    int s;
+
+    MinHeap(int arr[], int s){
+        this.arr = arr;
+        this.s=s;
+        for(int i = (s-2)/2;i>=0;i--){
+            minHeapify(i);
+        }
+    }
+
+    int parent(int i){
+        return (i-1)/2;
+    }
+
+    int left(int i){
+        return 2*i+1;
+    }
+
+    int right(int i){
+        return 2*i+2;
+    }
+
+    void minHeapify(int i){
+        int l = left(i);
+        int r = right(i);
+        int smallest = i;
+
+        if(l<this.s && arr[l]<arr[smallest]){
+            smallest = l;
+        }
+        if(r<this.s && this.arr[r]<this.arr[smallest]){
+            smallest = r;
+        }
+
+        if(smallest!=i){
+            int temp = arr[i];
+            arr[i]=arr[smallest];
+            arr[smallest]=temp;
+            minHeapify(smallest);
+        }
+    }
+
+    int extractMin(){
+        if(s==0){
+            return Integer.MAX_VALUE;
+        }
+        int root = arr[0];
+        if(s>1){
+            arr[0] = arr[s-1];
+            minHeapify(0);
+        }
+        s--;
+        return root;
+    }
+}
