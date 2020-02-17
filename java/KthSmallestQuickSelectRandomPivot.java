655b376c0177fca5f894e51a8a809ef04d4ea526
pushpanjay.kumar <pushpanjay.kumar@yatra.com>
2020-02-17 10:58:58 +0530

    kth smallest via random pivot quick select

--- java/KthSmallestQuickSelectRandomPivot.java
+++ java/KthSmallestQuickSelectRandomPivot.java
@@ -1,6 +1,8 @@
-
+/**
+ * @author pushpanjay.kumar created on 16/2/20
+ */
 // worst case O(n^2), but works in O(n) on average
-class KthSmallestQuickSelect{
+public class KthSmallestQuickSelectRandomPivot {
 
     private static void swap(Integer[] a, int i, int j){
         int temp = a[i];
@@ -19,11 +21,18 @@ class KthSmallestQuickSelect{
         }
         swap(arr, i+1, r);
         return i+1;
-    } 
+    }
+
+    private static int randomPartition(Integer[] a, int l, int r){
+        int n = r-l+1;
+        int pivot = (int)(Math.random() * (n-1));
+        swap(a, pivot+1, r);
+        return partition(a, l, r);
+    }
 
     private static int kthSmallest(Integer[] arr, int l, int r, int k){
         if(k>0 && k<=r-l+1 ){
-            int p = partition(arr, l, r);
+            int p = randomPartition(arr, l, r);
             if(p-l == k-1){
                 return arr[p];
             } else if(p-l > k-1){
@@ -38,6 +47,8 @@ class KthSmallestQuickSelect{
     public static void main(String[] args){
         Integer arr[] = new Integer[]{12, 3, 5, 7, 4, 19, 26};
         int k=3;
+        Math.random();
         System.out.println(kthSmallest(arr, 0, arr.length-1, k));
     }
+
 }
